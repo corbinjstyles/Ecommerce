ed878264d6916287e76b9252a4046b394e66e08c
ChristCrom <christcrom988@gmail.com>
2022-03-22 16:09:20 -0500

    Test

--- 02-back-end/spring-boot-ecommerce/src/main/java/com/hcl/springbootecommerce/SpringBootEcommerceApplication.java
+++ 02-back-end/spring-boot-ecommerce/src/main/java/com/hcl/springbootecommerce/SpringBootEcommerceApplication.java
@@ -9,5 +9,6 @@ public class SpringBootEcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 	}
+	//hello world
 
 }
