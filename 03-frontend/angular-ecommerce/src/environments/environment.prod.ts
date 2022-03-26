c490c380f45e348a798734020521a8cf27605db8
ChristCrom <christcrom988@gmail.com>
2022-03-26 18:07:33 -0500

    Fixing Production envirmonet

--- 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  HCLTechStoreUrl: "https://hcltechstore.azurewebsites.net/api",
+  stripePublishableKey:"pk_test_dPAj9z2XRlLfgAqPJgXNB4Ep"
 };
