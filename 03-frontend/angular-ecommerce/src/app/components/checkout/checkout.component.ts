0cf9bc896a156b877b630402a8d2193cd256745a
ChristCrom <christcrom988@gmail.com>
2022-03-31 19:03:26 -0500

    lol

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -215,8 +215,7 @@ this.cartItem = this.cartService.cartItems;
                 for(let i= 0; i < orderItems.length; i++ ){
                    this.products.getProduct(orderItems[i].productId).subscribe((result: any)=>
                    console.log(result))
-                   this.products.updateProduct(orderItems[i].productId, orderItems[i].quantity).subscribe((result)=>
-                   console.log(result))
+                   this.products.updateProduct(orderItems[i].productId, orderItems[i].quantity)
                 }
                 this.checkoutService.placeOrder(purchase).subscribe(
                   {
