f06ea0eda6182a5b76510a0dce0a57b7f2c9711d
ChristCrom <christcrom988@gmail.com>
2022-04-05 15:57:27 -0500

    Okta

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,8 +3,8 @@ export default {
     oidc:{
         clientId: '0oa43nbpf2qPTBzAH5d7',
         issuer: 'https://dev-8144678.okta.com/oauth2/default',
-        redirectUri: 'https://hcltechnologystore.azurewebsites.net/login/callback', 
-        scopes: ['openid', 'profile', 'email']
+        redirectUri: 'https://hcltechnologystore.azurewebsites.net/login/callback',
+        scopes: ['openid', 'profile', 'email', 'group']
     }
 
 }
